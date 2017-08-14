cf19c41c9c6ae79c13e482d11fdba017aa735925
Libin Lu <llu@evolenthealth.com>
2017-08-15 01:22:52 +0800

    remove function for RN 0.47

--- android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
+++ android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
@@ -24,11 +24,6 @@ public class FIRAnalyticsPackage implements ReactPackage {
         return modules;
     }
 
-    // Deprecated RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
