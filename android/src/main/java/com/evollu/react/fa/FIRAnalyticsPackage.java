0a3d641270e9b0b863a0c0ca0a205fea8935b2de
Libin Lu <libinlu89@gmail.com>
2017-08-15 01:40:43 +0800

    Revert "remove function for RN 0.47"
    This reverts commit cf19c41c9c6ae79c13e482d11fdba017aa735925.

--- android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
+++ android/src/main/java/com/evollu/react/fa/FIRAnalyticsPackage.java
@@ -24,6 +24,11 @@ public class FIRAnalyticsPackage implements ReactPackage {
         return modules;
     }
 
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
